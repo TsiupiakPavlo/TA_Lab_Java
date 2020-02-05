aa08fca7f51645e35392915c6fd09a961f476d85
Pavlo Tsiupiak <tsiupiak.p@gmail.com>
2020-02-05 02:00:18 +0200

    9_CollectionsEnum Menu ADD

--- 8_Generics/src/main/java/com/ta/java/epam/StringToInteger/Main.java
+++ 8_Generics/src/main/java/com/ta/java/epam/StringToInteger/Main.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        list.add("String");
-        System.out.println(list);
+        try {
+            list.add("String");
+            System.out.println(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
